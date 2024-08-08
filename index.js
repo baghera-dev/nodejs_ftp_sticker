1bfdf56c9c4100a85111e75fdebb2ccfb5f0c989
Anton-Tretiak <tretiak.ap@gmail.com>
2024-08-08 13:18:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,40 +8,28 @@ const axios = require('axios');
 const FormData = require('form-data');
 const { Readable } = require('stream');
 
-
 const app = express();
 
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.post('/upload', async (req, res) => {
   try {
-    // console.log(req.body);
-    // {
-    //     "orderId": ,
-    //     "lineItems": [
-    //         {
-    //             "_sticker_image": "",
-    //             "_sticker_product_sku": ""
-    //         }
-    //     ]
-    // }
-
     const orderId = req.body.orderId;
     const arr = req.body.lineItems.filter(el => el._sticker_image);
 
     for (let i = 0; i < arr.length; i++) {
       const base64Data = arr[i]._sticker_image.replace(/^data:image\/\w+;base64,/, "");
-      const decodedImage = Buffer.from(base64Data, 'base64');
-      const stream = Readable.from(decodedImage);
-  
-      
-      const payload = {
-        fileName: `order-${orderId}__sku-${arr[i]._sticker_product_sku}.jpg`,
-        mimeType: 'image/jpeg',
-        imageData: stream
-      };
+      const buffer = Buffer.from(base64Data, 'base64');
 
-      await axios.post('https://script.google.com/macros/s/AKfycbw5cu9nh-xxd2BXJY6ZxQpZnMmeXFu_c0ErpWqpMqMHg3xTSMLHTgQ2E7gBAfEVuG36fg/exec', payload)
+      const form = new FormData();
+      form.append('file', buffer, {
+        filename: `order-${orderId}__sku-${arr[i]._sticker_product_sku}.jpg`,
+        contentType: 'image/jpeg'
+      });
+
+      await axios.post('https://script.google.com/macros/s/AKfycbw5cu9nh-xxd2BXJY6ZxQpZnMmeXFu_c0ErpWqpMqMHg3xTSMLHTgQ2E7gBAfEVuG36fg/exec', form, {
+        headers: form.getHeaders()
+      })
         .then(response => {
           console.log('Image uploaded successfully:', response.data);
         })
